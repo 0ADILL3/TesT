725c0b7f7180f26fda2249662d5c45401016dc56
Adill Deswal Agi Firmansyah <adilllfirman@gmail.com>
2026-06-23 02:06:19 +0700

    hapus komentar

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,24 @@
-#include <Arduino.h> // Wajib ditambahkan di PlatformIO (berbeda dengan Arduino IDE)
+#include <Arduino.h>
 
-// Pin LED bawaan pada sebagian besar ESP32 DevKit adalah pin 2
 #define LED_PIN 2
 
-void setup() {
-  // Memulai komunikasi serial dengan kecepatan 115200 bps
+void setup()
+{
   Serial.begin(115200);
   
-  // Mengatur pin LED sebagai jalur keluar (Output)
   pinMode(LED_PIN, OUTPUT);
   
-  // Memberi jeda sebentar agar Serial Monitor siap
   delay(1000); 
   Serial.println("\n--- ESP32 Berhasil Menyala! ---");
 }
 
-void loop() {
-  digitalWrite(LED_PIN, HIGH);  // Menyalakan LED
+void loop()
+{
+  digitalWrite(LED_PIN, HIGH);
   Serial.println("Status: LED Nyala");
-  delay(1000);                  // Menunggu 1 detik (1000 milidetik)
+  delay(1000);
   
-  digitalWrite(LED_PIN, LOW);   // Mematikan LED
+  digitalWrite(LED_PIN, LOW);
   Serial.println("Status: LED Mati");
-  delay(1000);                  // Menunggu 1 detik
+  delay(1000);
 }
