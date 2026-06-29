e6907c144ab1d89040fe167a192be762b1ae012c
Adill Deswal Agi Firmansyah <adilllfirman@gmail.com>
2026-06-29 16:22:25 +0700

    json

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,9 @@
 #include <WiFiManager.h>
 #include <PubSubClient.h>
 #include <StepRunner.h>
+#include <ArduinoJson.h>
 
 ModbusMaster node;
-
 WiFiClient ESP32_S3_Client;
 PubSubClient client_MQTT(ESP32_S3_Client);
 
@@ -14,29 +14,29 @@ const int   MQTT_port     = 1883;
 const char* MQTT_username = "adill";
 const char* MQTT_password = "00111100";
 
-float temperature, humidity = 0.00;
+float temperature = 0.00;
+float humidity    = 0.00;
 
 void connect_MQTT()
 {
-  while (!client_MQTT.connected())
+  if (client_MQTT.connected()) {return;}
+
+  String client_ID = "ESP32-S3-Temperature_and_Humidity-RS-WS-N01-6";
+
+  Serial.println();
+  Serial.println((WiFi.status() == WL_CONNECTED) ? "WiFi Connected" : "WiFi Disconnected");
+  Serial.print("Connecting to MQTT...");
+
+  if (client_MQTT.connect(client_ID.c_str(), MQTT_username, MQTT_password))
   {
-    String client_ID = "ESP32-S3-Temperature_and_Humidity-RS-WS-N01-6";
-
-    Serial.println();
-    Serial.println((WiFi.status() == WL_CONNECTED) ? "WiFi Connected" : "WiFi Disconnected");
-    Serial.print("Connecting to MQTT...");
-
-    if (client_MQTT.connect(client_ID.c_str(), MQTT_username, MQTT_password))
-    {
-      Serial.println("OK");
-      // client_MQTT.subscribe("TesT/ESP32-S3/control");
-    }
-    else
-    {
-      Serial.print("Failed connect to MQTT. rc=");
-      Serial.println(client_MQTT.state());
-      delay(3000);
-    }
+    Serial.println("OK");
+    // client_MQTT.subscribe("TesT/ESP32-S3/control");
+  }
+  else
+  {
+    Serial.print("Failed connect to MQTT. rc=");
+    Serial.println(client_MQTT.state());
+    delay(3000);
   }
 }
 
@@ -53,8 +53,15 @@ void callback_MQTT(char* topic, byte* payload, unsigned int length)
 
 void publish_MQTT()
 {
-  client_MQTT.publish("TesT/ESP32-S3/Temperature", String(temperature, 2).c_str());
-  client_MQTT.publish("TesT/ESP32-S3/Humidity", String(humidity, 2).c_str());
+  JsonDocument doc;
+
+  char payload[128];
+  doc["TEMP"] = temperature;
+  doc["HUM"] = humidity;
+  
+  size_t payload_length = serializeJson(doc, payload);
+
+  client_MQTT.publish("TesT/ESP32-S3/data", payload, payload_length);
 }
 
 void Temperature_Humidity_sensor()
@@ -87,7 +94,7 @@ StepRunner Temperature_Humidity_sensor_task(Temperature_Humidity_sensor, 5000);
 void setup()
 {
   Serial.begin(115200);
-
+  
   Serial2.begin(4800, SERIAL_8N1, 16, 17);
   node.begin(1, Serial2);   // Slave ID = 1
 
